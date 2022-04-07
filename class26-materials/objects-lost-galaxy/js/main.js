e66a9274889c98d9d901bc482f3909227d32d16e
leerivera <leerivera80@gmail.com>
2022-04-07 19:56:02 -0400

    pizzas

--- class26-materials/objects-lost-galaxy/js/main.js
+++ class26-materials/objects-lost-galaxy/js/main.js
@@ -1,19 +1,17 @@
 //Create a dog object that has four properties and three methods
-function pizza() {
-    pizza.size = "large"
-    pizza.toppings= "3"
-    pizza.name= "supreme"
+let pizza = {}
+    pizza.size = "large",
+    pizza.toppings= "3",
+    pizza.name= "supreme",
 
-    function cutPizza(){
-        console.log("slice")
+    pizza.eatPizza = function(){
+        console.log('nomnom')
     }
 
-    function eatPizza(){
-        console.log("nom nom")
-    }
+   pizza.cutPizza = function(){
+       console.log("slice")
+   }
 
-    function buyPizza(){
-        console.log("7.99")
+    pizza.buyPizza = function(){
+        console.log("11.99")
     }
-
-}
